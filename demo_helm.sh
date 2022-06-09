c6bf6dd715aac7e693455b6d733997a8d2ff890e
Bharath <bharath.dasaraju@advancegroup.com>
2022-06-10 07:17:21 +0800

    know about helm not a big deal

--- demo_helm.sh
+++ demo_helm.sh
@@ -1,2 +1,18 @@
 how to work with helm
 
+a package manager for kubernetes
+
+Helm V3 - search, Commands
+
+
+helm charts:
+------------------>
+tiller removed.
+
+Deployment Service Secret
+
+More complex deployments handled with helm charts
+
+helm install -name mychart ./mychart
+
+
